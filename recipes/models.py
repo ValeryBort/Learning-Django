8612d24056824053638a83ddc7884409bd8968e6
Valery Bortnikova <valery.ivlieva@gmail.com>
2019-11-19 16:50:18 +0300

    Add IngredientList model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -6,3 +7,13 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class IngredientList(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    creation_date = models.DateTimeField()
+    ingredients = models.ManyToManyField(Ingredient)
+
+    def __str__(self):
+        return f'{self.user}: {self.name}'
